ef999157bde660f91a7c38e4fe5a13b644314acf
James Cassidy <jecassidy@sbgtv.com>
2020-09-18 14:06:16 -0700

    adding instance information

--- ec2-do/snapshot.py
+++ ec2-do/snapshot.py
@@ -8,7 +8,13 @@ ec2 = session.resource('ec2')
 def list_instances():
     "List ec2 instances"
     for i in ec2.instances.all():
-        print(i)
+        print(', '.join((
+            i.image_id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name
+        )))
 
 if __name__ == '__main__':
     list_instances()
