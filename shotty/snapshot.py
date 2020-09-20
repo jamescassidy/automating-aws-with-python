afc9a57911c59890437d0abb01d8400a2b96233a
James Cassidy <jecassidy@sbgtv.com>
2020-09-19 19:10:35 -0700

    Adding reboot option

--- shotty/snapshot.py
+++ shotty/snapshot.py
@@ -167,5 +167,23 @@ def list_instances(project):
 
     return
 
+@instances.command('reboot')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
+    "Reboot ec2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Rebooting {0}...".format(i.id))
+        try:
+            i.reboot()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not reboot {0}. ".format(i.id) + str(e))
+            continue
+
+    return
+
 if __name__ == '__main__':
     cli()
